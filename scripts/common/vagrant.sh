a69260f589c8d340c38a265569c7a850dd0c8460
DaBeOps <dberaud@dawawn.fr>
2018-05-23 12:09:48 +0200

    [Debug] fix the url with the correct filename & the directory

--- scripts/common/vagrant.sh
+++ scripts/common/vagrant.sh
@@ -6,5 +6,5 @@ set -x
 date | sudo tee /etc/vagrant_box_build_time
 
 mkdir -m 0700 -p ~/.ssh
-curl -fsSLo ~/.ssh/authorized_keys https://raw.githubusercontent.com/DaBeOps/packer-ssh/master/keys/formation_rsa.pub
+curl -fsSLo ~/.ssh/authorized_keys https://raw.githubusercontent.com/DaBeOps/packer-ssh/master/key/formation_rsa.pub
 chmod 600 ~/.ssh/authorized_keys
